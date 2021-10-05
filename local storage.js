f0c84d1207fae5bc79aeeec97cfdaaff0a8fcff1
aarondebarron <aoahorizon@gmail.com>
2021-10-05 02:01:26 +0000

    a vanilla Js african buffet app

---
+++ local storage.js
@@ -0,0 +1,18 @@
+
+
+
+
+// function deleteTodo(event){
+//     const item = e.target;
+
+//     if (item.classList[0] === "deleteItem"{
+//           // e.target.parentElement.remove();
+//    const todo = item.parentElement;
+//    todo.classList.add("fall");
+//    //at the end
+//    removeLocalTodos(todo);
+//    todo.addEventListener("transitionend", e => {
+//      todo.remove();
+//    });
+//  }
+// }
